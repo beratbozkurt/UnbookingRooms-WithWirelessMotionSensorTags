a873609575d3a1dbb3ceb3edcaa37d77f16e748a
beratbozkurt <brtbzkrt06@gmail.com>
2021-08-04 13:52:45 +0200

    Fetch All Tags From API with py script

---
+++ FetchTags.py
@@ -0,0 +1,16 @@
+import wirelesstagpy
+import json
+
+api = wirelesstagpy.WirelessTags(
+    username="berat.bozkurt@cloudica.io", password="1798Berat"
+)
+tags = api.load_tags()
+
+for (uuid, tag) in tags.items():
+    print("Motion Number: ", tag.motion_state)
+    print("uuid: ", uuid)
+    print(
+        "Loaded tag: {}, temp: {}, humidity: {} probe taken: {}".format(
+            tag.name, tag.temperature, tag.humidity, tag.time_since_last_update
+        )
+    )
